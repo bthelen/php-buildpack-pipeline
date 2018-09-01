f4358a385605c651e620915fd714ac324f2f0a11
Bruce E. Thelen <bthelen@pivotal.io>
2018-09-01 10:32:40 -0500

    trying to see if relative paths must be respected to find the oracle libraries

--- tasks/create-binaries.sh
+++ tasks/create-binaries.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-find .
+# find .
 
 # extract oracle drivers and sym link
 pushd oracle-drivers
@@ -13,6 +13,8 @@ popd
 
 #change to the binary builder directory and build php binaries
 pushd binary-builder-source
+ln -s . /binary-builder
+cd /binary-builder
 wget https://raw.githubusercontent.com/cloudfoundry/public-buildpacks-ci-robots/master/binary-builds/php7-extensions.yml
 ./bin/binary-builder --name=php7 --version=7.0.9 --md5=32ea3ce54d7d5ed03c6c600dffd65813  --php-extensions-file=./php7-extensions.yml
 tar ztvf *.tgz
