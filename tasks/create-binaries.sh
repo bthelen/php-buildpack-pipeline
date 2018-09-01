5557aaa03136f77ff13c5bbdf7143cd398a1e9a1
Bruce E. Thelen <bthelen@pivotal.io>
2018-09-01 10:51:46 -0500

    debugging paths

--- tasks/create-binaries.sh
+++ tasks/create-binaries.sh
@@ -10,14 +10,14 @@ pwd
 pushd oracle-drivers
 pwd
 tar zxvf oracle*tar.gz
-ln -s . /oracle
+ln -s `pwd` /oracle
 find /oracle
 popd
 
 #change to the binary builder directory and build php binaries
 pushd binary-builder-source
 pwd
-ln -s . /binary-builder
+ln -s `pwd` /binary-builder
 cd /binary-builder
 wget https://raw.githubusercontent.com/cloudfoundry/public-buildpacks-ci-robots/master/binary-builds/php7-extensions.yml
 ./bin/binary-builder --name=php7 --version=7.0.9 --md5=32ea3ce54d7d5ed03c6c600dffd65813  --php-extensions-file=./php7-extensions.yml
