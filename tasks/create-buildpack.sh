47fffd5cf1419dc05d4b64010f41f1ce9f942f40
Bruce E. Thelen <bthelen@pivotal.io>
2018-09-01 12:48:57 -0500

    Adding buildpack task

---
+++ tasks/create-buildpack.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -xe
+
+pwd
+
+pushd php-buildpack-source
+#Will need to edit the manifest.yml
+BUNDLE_GEMFILE=cf.Gemfile bundle
+BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager --uncached --stack=cflinuxfs2
