8d872ac4ba24378975247c9085654ed05d744159
Bruce E. Thelen <bthelen@pivotal.io>
2018-09-10 11:16:49 -0500

    Change sed syntax

--- tasks/create-buildpack.sh
+++ tasks/create-buildpack.sh
@@ -13,7 +13,7 @@ PHP_5_BINARY_SHA256=$(sha256sum $PHP_5_BINARY_LOCATION | awk '{print $1}')
 cp ../php-pipeline-source/manifest.yml .
 #PHP5 Section
 sed -i s/'##php-5-version##'/$PHP_5_VERSION/g manifest.yml
-sed -i s/'##php-5-binary-location##'/$PHP_5_BINARY_LOCATION/g manifest.yml
+sed -i s/'##php-5-binary-location##'/'$PHP_5_BINARY_LOCATION'/g manifest.yml
 sed -i s/'##php-5-binary-sha##'/$PHP_5_BINARY_SHA256/g manifest.yml
 cat manifest.yml
 #PHP7 Section
