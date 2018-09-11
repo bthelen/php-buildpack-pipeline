ad0b5af099e2910384a23528eea7d4c7d36ffe12
Bruce E. Thelen <bthelen@pivotal.io>
2018-09-10 20:44:23 -0500

    Fix escaping

--- tasks/create-buildpack.sh
+++ tasks/create-buildpack.sh
@@ -10,15 +10,15 @@ pushd php-buildpack-source
 #Get our custom version of manifest.yml
 cp ../php-pipeline-source/manifest.yml .
 #PHP5 Section
-PHP_5_BINARY_LOCATION_ESCAPED='\/php-binaries\/php-$PHP_5_VERSION-linux-x64.tgz'
-PHP_5_BINARY_LOCATION=/php-binaries/php-$PHP_5_VERSION-linux-x64.tgz
+PHP_5_BINARY_LOCATION_ESCAPED="\/php-binaries\/php-$PHP_5_VERSION-linux-x64.tgz"
+PHP_5_BINARY_LOCATION="/php-binaries/php-$PHP_5_VERSION-linux-x64.tgz"
 PHP_5_BINARY_SHA256=$(sha256sum $PHP_5_BINARY_LOCATION | awk '{print $1}')
 sed -i s/'##php-5-version##'/$PHP_5_VERSION/g manifest.yml
 sed -i s/'##php-5-binary-location##'/$PHP_5_BINARY_LOCATION_ESCAPED/g manifest.yml
 sed -i s/'##php-5-binary-sha##'/$PHP_5_BINARY_SHA256/g manifest.yml
 #PHP7 Section
-PHP_7_BINARY_LOCATION_ESCAPED='\/php-binaries\/php-$PHP_7_VERSION-linux-x64.tgz'
-PHP_7_BINARY_LOCATION=/php-binaries/php-$PHP_7_VERSION-linux-x64.tgz
+PHP_7_BINARY_LOCATION_ESCAPED="\/php-binaries\/php-$PHP_7_VERSION-linux-x64.tgz"
+PHP_7_BINARY_LOCATION="/php-binaries/php-$PHP_7_VERSION-linux-x64.tgz"
 PHP_7_BINARY_SHA256=$(sha256sum $PHP_7_BINARY_LOCATION | awk '{print $1}')
 sed -i s/'##php-7-version##'/$PHP_7_VERSION/g manifest.yml
 sed -i s/'##php-7-binary-location##'/$PHP_7_BINARY_LOCATION_ESCAPED/g manifest.yml
