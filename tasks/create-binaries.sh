4b58d94dc23f56d3148ccfc609715d676ca13d5b
Bruce E. Thelen <bthelen@pivotal.io>
2018-11-20 15:44:03 -0600

    No more need for hack, checksum was fixed

--- tasks/create-binaries.sh
+++ tasks/create-binaries.sh
@@ -19,13 +19,9 @@ popd
 pushd /binary-builder
 #Build a 5.6
 wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/build-binary-new/php-extensions.yml
-#sed -i.bak s/6d565680a4af0d2e261abbc3e3431b2b/3ae916ada628e4498b4f7de2f492fc6b/g php-extensions.yml
-sed -i.bak s/0.13.0/0.14.0/g php-extensions.yml
 ./bin/binary-builder --name=php --version=$PHP_5_VERSION --md5=ae625e0cfcfdacea3e7a70a075e47155 --php-extensions-file=./php-extensions.yml
 #Build a 7.1
 wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/build-binary-new/php7-extensions.yml
-#sed -i.bak s/6d565680a4af0d2e261abbc3e3431b2b/3ae916ada628e4498b4f7de2f492fc6b/g php7-extensions.yml
-sed -i.bak s/0.13.0/0.14.0/g php7-extensions.yml
 ./bin/binary-builder --name=php7 --version=$PHP_7_VERSION --md5=32ea3ce54d7d5ed03c6c600dffd65813 --php-extensions-file=./php7-extensions.yml
 popd
 
