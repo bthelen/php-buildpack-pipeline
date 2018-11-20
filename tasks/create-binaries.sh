6a0e72fa8ecdcbdcb40a80238ec864c6614733aa
Bruce E. Thelen <bthelen@pivotal.io>
2018-11-20 09:34:52 -0600

    Update URLs for the php extensions file.

--- tasks/create-binaries.sh
+++ tasks/create-binaries.sh
@@ -18,10 +18,10 @@ popd
 
 pushd /binary-builder
 #Build a 5.6
-wget https://raw.githubusercontent.com/cloudfoundry/public-buildpacks-ci-robots/master/binary-builds/php-extensions.yml
+wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/build-binary-new/php-extensions.yml
 ./bin/binary-builder --name=php --version=$PHP_5_VERSION --md5=ae625e0cfcfdacea3e7a70a075e47155 --php-extensions-file=./php-extensions.yml
 #Build a 7.1
-wget https://raw.githubusercontent.com/cloudfoundry/public-buildpacks-ci-robots/master/binary-builds/php7-extensions.yml
+wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/build-binary-new/php7-extensions.yml
 ./bin/binary-builder --name=php7 --version=$PHP_7_VERSION --md5=32ea3ce54d7d5ed03c6c600dffd65813 --php-extensions-file=./php7-extensions.yml
 popd
 
