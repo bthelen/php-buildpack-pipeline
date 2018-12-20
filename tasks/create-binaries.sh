a1b685a05b41c59559b08a8b6d5bb729e8dee8a5
Bruce E. Thelen <bthelen@pivotal.io>
2018-12-20 07:51:57 -0600

    Remove the manual package installation to see if pipeline works

--- tasks/create-binaries.sh
+++ tasks/create-binaries.sh
@@ -2,13 +2,6 @@
 
 set -xe
 
-#TODO - remove later
-#Modify the packages installed on this container because the same task inside of
-#binary-builder fails for some reason
-apt-get update
-apt-get -y upgrade
-apt-get -y install libffi-dev libaspell-dev libc-client2007e-dev libexpat1-dev libgdbm-dev libgmp-dev libgpgme11-dev libjpeg-dev libldap2-dev libmcrypt-dev libpng-dev libpspell-dev libsasl2-dev libsnmp-dev libsqlite3-dev libtool libxml2-dev libzip-dev libzookeeper-mt-dev snmp-mibs-downloader automake libgeoip-dev libtidy-dev libenchant-dev firebird-dev librecode-dev freetds-dev libgearman-dev libsybdb5 libreadline-dev libedit-dev libssl-dev libcurl4-openssl-dev
-
 # extract oracle drivers and sym link
 pushd oracle-drivers
 pwd
