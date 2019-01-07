10614643b052e7c0dd5c37a48777006228139efa
Bruce E. Thelen <bthelen@pivotal.io>
2019-01-07 12:51:38 -0600

    Must use output before putting the resource

--- tasks/create-binaries.sh
+++ tasks/create-binaries.sh
@@ -23,7 +23,12 @@ wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/
 #Build a 7.1
 wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/build-binary-new/php7-extensions.yml
 ./bin/binary-builder --name=php7 --version=$PHP_7_VERSION --md5=32ea3ce54d7d5ed03c6c600dffd65813 --php-extensions-file=./php7-extensions.yml
-#Make an archive with both of those in it.
-sha256sum *.tgz
-zip php_binaries-v$(date +%Y%m%d%H%M%S).zip *.tgz
 popd
+
+pushd binary-builder-source
+cp *.tgz ../php-binaries-built/
+popd
+
+pushd php-binaries-built
+zip php_binaries-v$(date +%Y%m%d%H%M%S).zip *.tgz
+sha256sum *
