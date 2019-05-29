cdcfe90bbb5b5ed108fab4badb1ca74b39e56c59
Bruce E. Thelen <bthelen@pivotal.io>
2019-05-29 08:01:06 -0500

    Build PHP 7.2 and 7.3 as well

--- tasks/create-binaries.sh
+++ tasks/create-binaries.sh
@@ -17,21 +17,17 @@ ln -s `pwd` /binary-builder
 popd
 
 pushd /binary-builder
-##Build a 5.6
-#wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/build-binary-new/php-extensions.yml
-#./bin/binary-builder --name=php --version=$PHP_5_VERSION --md5=ae625e0cfcfdacea3e7a70a075e47155 --php-extensions-file=./php-extensions.yml
-
 #Build a 7.1
 cp ../php-pipeline-source/php71-extensions.yml .
 ./bin/binary-builder --name=php7 --version=$PHP_7_1_VERSION --md5=32ea3ce54d7d5ed03c6c600dffd65813 --php-extensions-file=./php71-extensions.yml
 
 #Build a 7.2
-#wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/build-binary-new/php72-extensions.yml
-#./bin/binary-builder --name=php7 --version=$PHP_7_2_VERSION --md5=32ea3ce54d7d5ed03c6c600dffd65813 --php-extensions-file=./php72-extensions.yml
+cp ../php-pipeline-source/php72-extensions.yml .
+./bin/binary-builder --name=php7 --version=$PHP_7_2_VERSION --md5=32ea3ce54d7d5ed03c6c600dffd65813 --php-extensions-file=./php72-extensions.yml
 
 #Build a 7.3
-# wget https://raw.githubusercontent.com/cloudfoundry/buildpacks-ci/develop/tasks/build-binary-new/php72-extensions.yml
-#./bin/binary-builder --name=php7 --version=$PHP_7_3_VERSION --md5=32ea3ce54d7d5ed03c6c600dffd65813 --php-extensions-file=./php72-extensions.yml
+cp ../php-pipeline-source/php73-extensions.yml .
+./bin/binary-builder --name=php7 --version=$PHP_7_3_VERSION --md5=32ea3ce54d7d5ed03c6c600dffd65813 --php-extensions-file=./php73-extensions.yml
 
 popd
 
--- tasks/create-buildpack-cflinuxfs3.sh
+++ tasks/create-buildpack-cflinuxfs3.sh
@@ -11,14 +11,6 @@ pushd php-buildpack-source
 #Get our custom version of manifest.yml
 cp ../php-pipeline-source/manifest.yml .
 
-##PHP5 Section
-#PHP_5_BINARY_LOCATION_ESCAPED="\/php-binaries\/php-$PHP_5_VERSION-linux-x64.tgz"
-#PHP_5_BINARY_LOCATION="/php-binaries/php-$PHP_5_VERSION-linux-x64.tgz"
-#PHP_5_BINARY_SHA256=$(sha256sum $PHP_5_BINARY_LOCATION | awk '{print $1}')
-#sed -i s/'##php-5-version##'/$PHP_5_VERSION/g manifest.yml
-#sed -i s/'##php-5-binary-location##'/$PHP_5_BINARY_LOCATION_ESCAPED/g manifest.yml
-#sed -i s/'##php-5-binary-sha##'/$PHP_5_BINARY_SHA256/g manifest.yml
-
 #PHP7.1 Section
 PHP_7_1_BINARY_LOCATION_ESCAPED="\/php-binaries\/php7-$PHP_7_1_VERSION-linux-x64.tgz"
 PHP_7_1_BINARY_LOCATION="/php-binaries/php7-$PHP_7_1_VERSION-linux-x64.tgz"
@@ -28,20 +20,20 @@ sed -i s/'##php-7-1-binary-location##'/$PHP_7_1_BINARY_LOCATION_ESCAPED/g manife
 sed -i s/'##php-7-1-binary-sha##'/$PHP_7_1_BINARY_SHA256/g manifest.yml
 
 #PHP7.2 Section
-#PHP_7_2_BINARY_LOCATION_ESCAPED="\/php-binaries\/php7-$PHP_7_2_VERSION-linux-x64.tgz"
-#PHP_7_2_BINARY_LOCATION="/php-binaries/php7-$PHP_7_2_VERSION-linux-x64.tgz"
-#PHP_7_2_BINARY_SHA256=$(sha256sum $PHP_7_2_BINARY_LOCATION | awk '{print $1}')
-#sed -i s/'##php-7-2-version##'/$PHP_7_2_VERSION/g manifest.yml
-#sed -i s/'##php-7-2-binary-location##'/$PHP_7_2_BINARY_LOCATION_ESCAPED/g manifest.yml
-#sed -i s/'##php-7-2-binary-sha##'/$PHP_7_2_BINARY_SHA256/g manifest.yml
+PHP_7_2_BINARY_LOCATION_ESCAPED="\/php-binaries\/php7-$PHP_7_2_VERSION-linux-x64.tgz"
+PHP_7_2_BINARY_LOCATION="/php-binaries/php7-$PHP_7_2_VERSION-linux-x64.tgz"
+PHP_7_2_BINARY_SHA256=$(sha256sum $PHP_7_2_BINARY_LOCATION | awk '{print $1}')
+sed -i s/'##php-7-2-version##'/$PHP_7_2_VERSION/g manifest.yml
+sed -i s/'##php-7-2-binary-location##'/$PHP_7_2_BINARY_LOCATION_ESCAPED/g manifest.yml
+sed -i s/'##php-7-2-binary-sha##'/$PHP_7_2_BINARY_SHA256/g manifest.yml
 
 #PHP7.3 Section
-#PHP_7_3_BINARY_LOCATION_ESCAPED="\/php-binaries\/php7-$PHP_7_3_VERSION-linux-x64.tgz"
-#PHP_7_3_BINARY_LOCATION="/php-binaries/php7-$PHP_7_3_VERSION-linux-x64.tgz"
-#PHP_7_3_BINARY_SHA256=$(sha256sum $PHP_7_3_BINARY_LOCATION | awk '{print $1}')
-#sed -i s/'##php-7-3-version##'/$PHP_7_3_VERSION/g manifest.yml
-#sed -i s/'##php-7-3-binary-location##'/$PHP_7_3_BINARY_LOCATION_ESCAPED/g manifest.yml
-#sed -i s/'##php-7-3-binary-sha##'/$PHP_7_3_BINARY_SHA256/g manifest.yml
+PHP_7_3_BINARY_LOCATION_ESCAPED="\/php-binaries\/php7-$PHP_7_3_VERSION-linux-x64.tgz"
+PHP_7_3_BINARY_LOCATION="/php-binaries/php7-$PHP_7_3_VERSION-linux-x64.tgz"
+PHP_7_3_BINARY_SHA256=$(sha256sum $PHP_7_3_BINARY_LOCATION | awk '{print $1}')
+sed -i s/'##php-7-3-version##'/$PHP_7_3_VERSION/g manifest.yml
+sed -i s/'##php-7-3-binary-location##'/$PHP_7_3_BINARY_LOCATION_ESCAPED/g manifest.yml
+sed -i s/'##php-7-3-binary-sha##'/$PHP_7_3_BINARY_SHA256/g manifest.yml
 
 cat manifest.yml
 gem install bundler -v '1.17.3'
