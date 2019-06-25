87df1306d38d743ad37063e44983f3bf7d2d137e
Bruce E. Thelen <bthelen@pivotal.io>
2019-06-25 13:02:31 -0500

    redoing the version resource

--- tasks/create-binaries.sh
+++ tasks/create-binaries.sh
@@ -19,10 +19,24 @@ pwd
 ln -s `pwd` /binary-builder
 popd
 
+#get the version from the file
+if [[ $PHP_VERSION = "71" ]]
+then
+    export PHP_VERSION_NUMBER=`cat php-71-version/php-71-version`
+elif [[ $PHP_VERSION = "72" ]]
+then
+    export PHP_VERSION_NUMBER=`cat php-72-version/php-72-version`
+elif [[ $PHP_VERSION = "73" ]]
+then
+    export PHP_VERSION_NUMBER=`cat php-73-version/php-73-version`
+else
+    exit 1
+fi
+
 pushd /binary-builder
 #Run the builder - for some reason the md5sum is required but not used
 cp ../php-pipeline-source/$EXTENSIONS_FILE .
-./bin/binary-builder --name=php7 --version=$PHP_VERSION --md5=irrelevant --php-extensions-file=./$EXTENSIONS_FILE
+./bin/binary-builder --name=php7 --version=$PHP_VERSION_NUMBER --md5=irrelevant --php-extensions-file=./$EXTENSIONS_FILE
 
 popd
 
